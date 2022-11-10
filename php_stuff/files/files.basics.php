956bc451088e02fda437dccb329de29f76b0b23e
Jankes <jankes@jankes.com.pl>
2022-11-10 13:20:04 +0100

    files

---
+++ php_stuff/files/files.basics.php
@@ -0,0 +1,5 @@
+<?php
+
+# file — Reads entire file contents into an array of lines.
+# file_get_contents — Reads entire file contents into a string.
+# fopen — Opens a file handle that can be manipulated with other library functions, but does no reading or writing itself.
