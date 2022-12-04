2989399acc8e06d1f2c66f073fd3a3112d245cb1
Jankes <jankes@jankes.com.pl>
2022-12-04 16:49:56 +0100

    JS: arrays

--- js/arrays.js
+++ js/arrays.js
@@ -1,1 +1,12 @@
 const myArray = [18, 64, 99];
+
+const arr = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [[10, 11, 12], 13, 14]
+];
+
+const subarray = arr[3];
+const nestedSubarray = arr[3][0];
+const element = arr[3][0][1];
