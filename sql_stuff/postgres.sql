57b92fdff5813bb538e8629952c179043aa9dd57
Jankes <jankes@jankes.com.pl>
2022-12-05 22:09:38 +0100

    Postgres

---
+++ sql_stuff/postgres.sql
@@ -0,0 +1,15 @@
+psql --username=freecodecamp --dbname=postgres //uruchomienie postgres
+
+\l // list databases
+
+CREATE DATABASE database_name;
+
+\c database_name // choose database
+
+\d // display tables
+
+CREATE TABLE table_name();
+
+\d table_name //display table info
+
+ALTER TABLE table_name ADD COLUMN column_name DATATYPE;
