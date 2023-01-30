de23a88142771af47f2bf7e8b43278fa45de3a67
Jankes <jankes@jankes.com.pl>
2023-01-30 22:11:26 +0100

    pierdy

--- check.php
+++ check.php
@@ -1,17 +1,2 @@
 <?php
 
-// $a = true;
-// $b = false;
-
-// $c = $a ?: 'n';
-
-// echo $c;
-
-$a = '1';
-$a = $a++;
-$b = &$a;
-$b = "2$b";
-
-echo $a;
-echo '<br>';
-echo $a . ", " . $b;
---
+++ php/mod_rewrite/htaccess.php
@@ -0,0 +1,3 @@
+<?php
+
+# link: https://www.youtube.com/watch?v=ytVPiYILz80
