8dcb54929f0676de3880e262f8f590997c3b928a
Jankes <jankes@jankes.com.pl>
2023-02-09 14:02:34 +0100

    helpers

---
+++ php/helpers.php
@@ -0,0 +1,14 @@
+<?php
+
+function prr($item) 
+{
+    echo '<pre>';
+    print_r($item);
+    echo'</pre>';
+}
+function vad($item) 
+{
+    echo '<pre>';
+    var_dump($item);
+    echo'</pre>';
+}
