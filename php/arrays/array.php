a4c63641e871f1ebe1be1652eb88bb20a2e49a77
Jankes <jankes@jankes.com.pl>
2023-02-09 14:02:50 +0100

    arrays

--- php/arrays/array.php
+++ php/arrays/array.php
@@ -1,2 +1,34 @@
 <?php
+require '../helpers.php';
+
+# man: https://www.php.net/manual/en/ref.array.php
+
+$array = ['jeden', 'dwa', 'trzy', 'cztery', 'pięć'];
+$array1 = ['a', 'b', 'c', 'd', 'e'];
+$array2 = [1, 2, 3, 4, 5];
+
+# dzieli tablice na mniejsze z podaną liczbą elemenów
+prr(array_chunk($array, 3));
+
+# łączy tablice, bierze klucze z pierwsze i wartości z drugiej
+prr(array_combine($array1, $array2));
+
+# przechodzi przez każdy element tablicy i wykonuje na nim funkcje
+//nie zmienia wartości, tylko filtruje
+$even = array_filter($array2, fn($value) => $value % 2 == 0);
+prr($even);
+
+# wyszukuje klucze w tablicy
+$klucz = array_keys($array, 'dwa'); // 1
+prr($klucz);
+
+# przechodzi przez tablicę i wykonuje na niej funkcje
+$doubledValues = array_map(fn ($var) => $var * 2, $array2);
+
+# listuje do zmiennych elementy tablicy
+list($a, $b, $c) = $array1;
+echo $a . $b . $c; // abc
+//to samo
+[$a1, $b1, $c1] = $array2;
+echo $a1 . $b1 . $c1; // 123
 
---
+++ php/arrays/array_passing.php
@@ -0,0 +1,4 @@
+<?php
+
+# https://stackoverflow.com/questions/3432257/difference-between-array-map-array-walk-and-array-filter
+
